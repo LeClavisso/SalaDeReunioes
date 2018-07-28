d094678b05fe984d3d5103ee2fa54c797c56bf13
LeClavisso <38593877+LeClavisso@users.noreply.github.com>
2018-07-28 15:43:43 -0300

    Testando

---
+++ SalaDeReunioes/Dia.cs
@@ -0,0 +1,100 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.IdGenerators;
+using MongoDB.Bson.Serialization.Options;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver.Core.Authentication.Sspi;
+using MongoDB.Driver.Core.Bindings;
+using MongoDB.Driver.Core.Clusters.ServerSelectors;
+using MongoDB.Driver.Core.Configuration;
+using MongoDB.Driver.Core.ConnectionPools;
+using MongoDB.Driver.Core.Connections;
+using MongoDB.Driver.Core.Events.Diagnostics;
+using MongoDB.Driver.Core.Misc;
+using MongoDB.Driver.Core.Operations.ElementNameValidators;
+using MongoDB.Driver.Core.Servers;
+using MongoDB.Driver.Core.WireProtocol.Messages.Encoders.BinaryEncoders;
+using MongoDB.Driver.Core.WireProtocol.Messages.Encoders.JsonEncoders;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace SalaDeReunioes
+{
+    public class Dia
+    {
+        public ObjectId Id { get; set; }
+        public string dia { get; set; }
+        public string hora { get; set; }
+        public string disponibilidade { get; set; }
+        public string pessoa { get; set; }
+
+
+                public void CriarTabelas()
+                 {
+            
+             string connectionString = "mongodb://localhost:27017";
+            var cliente = new MongoServer
+           // MongoClient client = new MongoClient(connectionString);
+           // IMongoDatabase db = client.GetDatabase("SalaDeReunioes");
+          //  var collection = db.GetCollection<Dia>("Dias");
+            var entity = new Dia()
+            {
+                dia = "Fulano",
+                hora = "hora",
+                disponibilidade = "dispo",
+                pessoa = "pesso"
+            };
+           // collection.InsertOne(entity);
+            //var id = entity.Id;
+
+
+            
+            /*List<Dia> lista = new List<Dia>();
+            
+            db.CreateCollection(name: "ListaDeHorarios");
+            db.GetCollection<Dia>("ListaDeHorarios");
+            Dia a = new Dia();
+
+            for (int d = 1; d <31; d++)
+            {
+                for (int i = 8; i < 18; i++)
+                {
+                    if (d < 10)
+                    {
+                        a.dia = "0" + d.ToString() + "07";
+                    }
+                    else
+                    {
+                        a.dia = d.ToString() + "/07";
+                    }
+                    a.hora = i.ToString() + "-" + (i + 1).ToString() + "h";
+                    a.disponibilidade = "livre";
+                    a.pessoa = "-";
+
+                    lista.Add(a);
+                    */
+                }
+                public void Coletar()
+        {
+          /*  string connectionString = "mongodb://localhost:27017";
+            MongoClient client = new MongoClient(connectionString);
+            IMongoDatabase db = client.GetDatabase("SalaDeReunioes");
+            var collection = db.GetCollection<Dia>("Dias");
+            var query = Queryable<Dia>.Equals(e => e.Id, Id);
+            var entity = collection.Find<Dia>(query);
+        }
+                
+            }
+
+            //horarios.InsertMany(lista);
+        
+        }
+    
---
+++ SalaDeReunioes/Global.asax.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using System.IO;
+using System.Data;
+
+namespace SalaDeReunioes
+{
+    public class Global : System.Web.HttpApplication
+    {
+        
+       
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            //Dia dia = new Dia();
+            //dia.CriarTabelas();
+
+                int count = 0;
+
+            string connectionString = "mongodb://localhost:27017";
+        
+       // MongoClient client = new MongoClient(connectionString);
+      //  IMongoDatabase db = client.GetDatabase("SalaDeReunioes");
+          
+
+           /* System.IO.StreamWriter file =new System.IO.StreamWriter(@"C:\Horarios.txt");
+                
+                foreach (Dia d in listaDias) {
+                
+
+                 file.WriteLine(d.dia + "/" + d.hora + "/" + d.disponibilidade + "/" + d.pessoa); 
+                  }
+            
+            file.Close();*/
+
+
+
+
+        } 
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
